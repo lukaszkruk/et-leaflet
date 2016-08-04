86d47808990c642617fcc084f7e640d261d35dee
lukaszkruk <LKRUK@IRQLKRUK-L.as.iom.net>
2016-08-04 15:57:34 +0300

    replaced osm with mapbox

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 var map = L.map('map').setView([34, 43], 6);
 
-L.tileLayer(
+/* L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {maxZoom: 18}).addTo(map);
+    {maxZoom: 18}).addTo(map); */
+	
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrYXN6a3J1ayIsImEiOiJjaWlyOHhna2gwMmp2dTFrcGhmY3Z5NWgzIn0.8J8YJkhUxbP6jmB6VK4RLw').addTo(map);	
 	
 var geojsonLayer = new L.GeoJSON();
 
@@ -11,7 +13,7 @@ function handleJson(data) {
 	geojsonLayer.addData(data);
 }
 
-//need to enable JSONP in geoserver first!
+//need to enable JSONP in geoserver first 
 
 $.ajax({
 	url: "http://localhost:8080/geoserver/ows?service=wfs&version=2.0.0&request=GetFeature&typeNames=workspace1:view&outputFormat=text/javascript&count=10&format_options=callback:getJson",
