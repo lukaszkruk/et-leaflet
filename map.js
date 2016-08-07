28a2ef01861c8d446c658ac3497d5dadf28aba1a
lukaszkruk <LKRUK@IRQLKRUK-L.as.iom.net>
2016-08-07 10:08:12 +0300

    changed basemap

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ var map = L.map('map').setView([34, 43], 6);
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
     {maxZoom: 18}).addTo(map); */
 	
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrYXN6a3J1ayIsImEiOiJjaWlyOHhna2gwMmp2dTFrcGhmY3Z5NWgzIn0.8J8YJkhUxbP6jmB6VK4RLw').addTo(map);	
+L.tileLayer('https://api.mapbox.com/styles/v1/lukaszkruk/cirgbygyt001bh5kp7bessgd4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVrYXN6a3J1ayIsImEiOiJjaWlyOHhna2gwMmp2dTFrcGhmY3Z5NWgzIn0.8J8YJkhUxbP6jmB6VK4RLw').addTo(map);	
 	
 var geojsonLayer = new L.GeoJSON();
 
